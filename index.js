b1bbeda7502abcd58acdf1630e10debc2d8316b4
goyalpankaj2005 <69196444+goyalpankaj2005@users.noreply.github.com>
2020-08-06 17:20:03 +0530

    added initial home page

---
+++ index.js
@@ -0,0 +1,7 @@
+/learn nav learn nav learn nav learn nav learn nav learn nav learn nav learn nav learn nav learn nav learn nav learn nav /
+function closelearnnav() {
+  document.getElementById("learn-nav").style.height = "0";
+}
+function openlearnnav() {
+  document.getElementById("learn-nav").style.height = "auto";
+}
